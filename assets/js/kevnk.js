c7b88521a46a4cdc89a7b0d8016aa976710786e6
Kevin Kirchner <kirchner.kevin@gmail.com>
2014-07-18 08:19:42 -0400

    fixing aspect ratio on blog list images for mobile

--- assets/js/kevnk.js
+++ assets/js/kevnk.js
@@ -21,4 +21,34 @@
     // Highlight code
     hljs.initHighlightingOnLoad();
 
+    // Maintain blog list image top
+    var fullPageWidth = 800;
+    var $blogListImgs = $('#blog ul.blog-posts img')
+    $blogListImgs.each(function(){
+        var $img = $(this);
+        var $par = $img.parent();
+        var imgTop = parseInt($img.css('top'), 10);
+        var parH = parseInt($par.css('height'), 10);
+        $img.data('top-aspect-ratio', imgTop / fullPageWidth);
+        $par.data('height-aspect-ratio', parH / fullPageWidth);
+    });
+
+    /**
+     * keep top style ratio to the original
+     * set parent height no higher than the image shown
+     */
+    var maintainImageTopAspect = function() {
+        $blogListImgs.each(function(){
+            var $img = $(this);
+            var newTop = $img.width() * $img.data('top-aspect-ratio');
+            $img.css('top', newTop);
+
+            var $par = $img.parent();
+            var newHeight = $par.width() * $par.data('height-aspect-ratio');
+            $par.css('height', newHeight);
+        });
+    }
+
+    $(window).on('resize', maintainImageTopAspect).trigger('resize');
+
 })(jQuery, this, document)
